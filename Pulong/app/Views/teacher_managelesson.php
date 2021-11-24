2d9995eb37c2c6ea76b640418ea6c0679e95a55b
Migz <apaga.miggy_vincent@hnu.edu.ph>
2021-11-24 21:52:59 +0800

    "Add Activity" added

--- Pulong/app/Views/teacher_managelesson.php
+++ Pulong/app/Views/teacher_managelesson.php
@@ -47,6 +47,17 @@
             </div>
 
             <div class="col-md text-center">
+                <div class="dashboard_div section addact" href="teacher_managelesson.html">
+                <br>
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="green" class="bi bi-pencil" viewBox="0 0 16 16">
+                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                </svg>
+                  <br><br>
+                  <p>Add Activity</p>
+                </div>
+            </div>
+
+            <div class="col-md text-center">
                 <div class="dashboard_div section viewmod" href="teacher_managelesson.html">
                 <br>
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="green" class="bi bi-file-earmark-easel" viewBox="0 0 16 16">
@@ -84,6 +95,7 @@
                   <p>Remove Module</p>
                 </div>
             </div>
+
         </div>
       </div>
     </div>
@@ -95,6 +107,11 @@
   });
 
   $(".addmod").click(function(){
+    window.location = 'addmodule';
+    //
+});
+
+  $(".addact").click(function(){
     window.location = 'addmodule';
     //
 });
@@ -117,7 +134,7 @@
   </script>
 
   <style>
-    .addmod:hover, .remmod, .viewmod, .upmod{
+    .addmod:hover, .remmod, .viewmod, .upmod ,.addact{
       cursor: pointer;
     }
   </style>
