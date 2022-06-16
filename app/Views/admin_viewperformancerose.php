51dc69de0130fdfa5d4ffc2c850c55c8b2f9c6d2
saladbut <lerin113@gmail.com>
2022-06-16 09:54:50 +0800

    'revisions2'

--- app/Views/admin_viewperformancerose.php
+++ app/Views/admin_viewperformancerose.php
@@ -1,5 +1,9 @@
 <?= $this->extend('layouts/main') ?>
 <?= $this->section('content') ?>
+<?php
+  $button='display';
+  $text='none';
+ ?>
 
 <div class="navbar" style="background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(<?=base_url()?>/public/assets/images/banner.png);">
        <nav class="nav row w-100 align-items-center">
@@ -70,27 +74,36 @@
              <?php if($users): ?>
              <?php foreach($users as $user): ?>
                <?php if ($user['section_id']==1): ?>
+                   <?php if (strcmp(strtoupper($user['account_status']),strtoupper('inactive'))==0): ?>
+                        <?php
+                          $text='display';
+                          $button='none';
+                         ?>
+                   <?php endif; ?>
                  <tr>
                    <td style="text-align:left"><?php echo $user['pupil_firstname']; ?> <?php echo $user['pupil_lastname']; ?> </td>
-                   <td><a href="<?php echo base_url(); ?>/public/admin/viewperformance_module\<?= $user['section_id']; ?>\<?= $user['pupil_id'] ?>" style="text-decoration:none;">
+                   <td><a href="<?php echo base_url(); ?>/public/admin/viewperformance_module\<?= $user['section_id']; ?>\<?= $user['pupil_id'] ?>" style="text-decoration:none;display:<?= $button; ?>">
                      <buttontype="button" class="btn btn-outline-success">
                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                             <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
                           </svg> View Performance</button>
                        </a>
-                       <a href="<?php echo base_url(); ?>/public/admin/view_overallperformance\<?= $user['pupil_id'] ?>" style="text-decoration:none;">
+                       <a href="<?php echo base_url(); ?>/public/admin/view_overallperformance\<?= $user['pupil_id'] ?>" style="text-decoration:none;display:<?= $button; ?>">
                          <buttontype="button" class="btn btn-outline-success">
                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
                               </svg> View Overall Performance</button>
                            </a>
-
+                        <h5 style="text-align:center;display:<?= $text; ?>;color:grey;">Pupil is Inactive</h5>
                    </td>
                   </tr>
                <?php endif; ?>
-
+               <?php
+                 $button='display';
+                 $text='none';
+                ?>
             <?php endforeach; ?>
             <?php endif; ?>
         </table>
